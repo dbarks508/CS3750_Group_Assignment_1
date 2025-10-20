644413947bfa1a7cb858ff687a4b07043bead612
Michael Rasmussen <michaelrasmussen5@mail.weber.edu>
2025-10-20 09:06:17 -0600

    added the gain/loss page

---
+++ frontend/src/components/postGamePage.js
@@ -0,0 +1,52 @@
+function toTimeElapsedStr(deltaTime){
+	let times = [
+		1_000,
+		60,
+		60,
+		24,
+	];
+
+	let prev = deltaTime;
+	for(let i = 0; i < times.length; i++){
+		prev /= times[i];
+		let out = Math.floor(prev);
+
+		if(i + 1 < times.length){
+			out %= times[i + 1];
+		}
+
+		times[i] = out;
+	}
+
+	return `${times.pop()} days, ${times.pop()} hours`;
+}
+
+function toMoneyStr(num){
+	let out = Math.floor(num).toString().split("").reverse();
+
+	let c_count = out.length / 3 - 1;
+	for(let i = 0; i < c_count; i++){
+		out.splice(i + 3 * (i + 1), 0, ",");
+	}
+	out = "$" + out.reverse().join("");
+
+	const DECIMALS = 2;
+	let offset = Math.pow(10, DECIMALS);
+	out += "." + (Math.floor(num * offset) % offset).toString().padStart(2, "0");
+
+	return out;
+}
+
+export default function PostGamePage({start, end, elapsed}){
+	let diff = end - start;
+	let gain = diff >= 0;
+
+	return (
+		<div>
+			<p>Elapsed period of time: {toTimeElapsedStr(elapsed)}</p>
+			<p>Starting amount: {toMoneyStr(start ?? 10_000)}</p>
+			<p>Ending amount: {toMoneyStr(end)}</p>
+			<p>{gain ? "Gain":"Loss"}: {gain ? "+":"-"}{toMoneyStr(Math.abs(diff))}</p>
+		</div>
+	);
+}
