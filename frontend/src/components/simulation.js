80bbba8a9bf470bdf0178c8c2540f48295b6764e
Chance McKay <144727799+Haywyure@users.noreply.github.com>
2025-10-20 15:29:04 -0600

    removed dupe

--- frontend/src/components/simulation.js
+++ frontend/src/components/simulation.js
@@ -51,7 +51,7 @@ function Simulation() {
 
             // send request to backend to get the next day data
             fetch('http://localhost:5000/next',{
-                method: 'GET',
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
@@ -113,66 +113,7 @@ function Simulation() {
                 return;
         }
 
-  async function handleButtonClick(button) {
-    console.log(`Button ${button} clicked`);
-    setButton(button);
-
-    if (button === "nextDayButton") {
-      console.log("moving to the next day");
-      day += 1; // increment the day
-
-      // send request to backend to get the next day data
-      fetch("http://localhost:5000/next", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      });
-    } else if (button === "buyButton") {
-      console.log("buying shares");
-      // get the input number from an input option, should be linked so any will do
-      let amountToBuy = selected_amount; // placeholder value
-
-      // send request to backend to buy the shares
-      fetch(`http://localhost:5000/buy/:${amountToBuy}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      });
-
-      console.log("get response and do stuff here");
-    } else if (button === "sellButton") {
-      console.log("selling shares");
-      // get the input number from an input option, should be linked so any will do
-      let amountToSell = 10; // placeholder value
-
-      // send request to backend to sell the shares
-      fetch(`http://localhost:5000/sell/:${amountToSell}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      });
-    } else if (button === "quitButton") {
-      // ensure that the day is at least 7
-      if (day > 6) {
-        // display stats
-        console.log("game ended");
-        // send request to backend to get final stats
-        fetch("http://localhost:5000/quit", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(),
-        });
-      } else return;
-    }
-  }
+  
 
   // end button logic ---------
 
