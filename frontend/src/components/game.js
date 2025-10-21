9bd591d1cce1ce1331b07898135d72322cbf8332
GarrettOtto <125316468+GarrettOtto@users.noreply.github.com>
2025-10-20 20:34:15 -0600

    Balance Page

--- frontend/src/components/game.js
+++ frontend/src/components/game.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import BalancePage from "./balancePage";
 
 function Game() {
+  // Declaring tickerSymbol, balance, day
   const [ticker_symbol, set_ticker_symbol] = useState("");
+  const [balance, setBalance] = useState(10000);
+  const [day, setDay] = useState(1)
+
   const navigate = useNavigate();
 
   async function on_ticker_submit(e) {
     e.preventDefault();
-
+    // TODO - make sure the inputted ticker is a valid symbol
     try {
       const response = await fetch("http://localhost:5000/stock", {
         method: "post",
@@ -29,17 +34,14 @@ function Game() {
   }
 
   return (
-    <div id="container">
-      <form onSubmit={on_ticker_submit}>
-        <label>Enter Ticker Symbol:</label>
-        <input
-          type="text"
-          value={ticker_symbol}
-          onChange={(e) => set_ticker_symbol(e.target.value)}
-        />
-        <button type="submit">View Stock</button>
-      </form>
-    </div>
+    // This is the entire HTML balance page that is pulling from balancePage.js
+    <BalancePage
+      on_ticker_submit={on_ticker_submit}
+      ticker_symbol={ticker_symbol}
+      set_ticker_symbol={set_ticker_symbol}
+      balance={balance}
+      day={day}
+    />
   );
 }
 
