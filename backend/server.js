bde05d09420bba5c42b1ab74ec1f0a8638da9f54
Michael Rasmussen <michaelrasmussen5@mail.weber.edu>
2025-10-22 17:53:50 -0600

    project clean up

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/records"));
 
-const port = process.env.PORT;
+const port = process.env.PORT ?? 5000;
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
